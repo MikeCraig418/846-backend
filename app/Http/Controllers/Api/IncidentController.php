a3c25347a5c807d2f7b1158d1f5d3aada140bd2e
Michael Craig <mcraig@bytelaunch.com>
2020-06-03 12:13:39 -0700

    feat(incidents): add Api controller with index

---
+++ app/Http/Controllers/Api/IncidentController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\IncidentResource;
+use App\Models\Incident;
+use Illuminate\Http\Request;
+
+class IncidentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return IncidentResource::collection(Incident::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Incident  $incident
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Incident $incident)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Incident  $incident
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Incident $incident)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Incident  $incident
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Incident $incident)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Incident  $incident
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Incident $incident)
+    {
+        //
+    }
+}
