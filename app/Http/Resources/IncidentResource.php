24b447f0b57978bb010f8d9654fb75ef606a6143
Michael Craig <mcraig@bytelaunch.com>
2020-06-05 09:21:28 -0700

    refactor(api): simply response logic (now refs model attributes)

--- app/Http/Resources/IncidentResource.php
+++ app/Http/Resources/IncidentResource.php
@@ -15,29 +15,7 @@ class IncidentResource extends JsonResource
     public function toArray($request)
     {
 
-        $links = [];
-        if ($this->link_1 != "") $links[] = $this->link_1;
-        if ($this->link_2) $links[] = $this->link_2;
-        if ($this->link_3) $links[] = $this->link_3;
-        if ($this->link_4) $links[] = $this->link_4;
-        if ($this->link_5) $links[] = $this->link_5;
-        if ($this->link_6) $links[] = $this->link_6;
-        if ($this->link_7) $links[] = $this->link_7;
+        return parent::toArray($request);
 
-        return [
-            'id' => $this->id,
-            'pr_id' => $this->pr_id,
-            'state' => $this->state,
-            'city' => $this->city,
-            'date' => $this->date,
-            'title' => $this->title,
-            'description' => $this->description,
-            'links' => $links,
-            'data' => $this->data,
-            'geocoding' => [
-                'lat' => $this->lat,
-                'long' => $this->long,
-            ]
-        ];
     }
 }
