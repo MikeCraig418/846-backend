3f5dddf1ac8432c8f47ee66bcec97cf21107dc1a
Michael Craig <mcraig@bytelaunch.com>
2020-06-05 09:18:27 -0700

    feat(incidents): add cli to import videos from 2020pb repo

---
+++ app/Console/Commands/ImportIncidents.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Evidence;
+use App\Models\Incident;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ImportIncidents extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pb:import-incidents';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import incidents from 2020PB Repo';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $source_url = "https://raw.githubusercontent.com/2020PB/police-brutality/data_build/all-locations.json";
+
+        $response = Http::get($source_url);
+
+        if ($incidents = $response->json()) {
+
+            foreach ($incidents['data'] as $row) {
+
+                $incident = new Incident();
+                $incident->state = $row['state'] ?? '';
+                $incident->city = $row['city'] ?? '';
+                $incident->title = $row['name'];
+                $incident->date = ($this->isValidForCarbon($row['date'])) ? $row['date'] : '1900-01-01';
+                $incident->links = $row['links'];
+                $incident->lat = 0;
+                $incident->long = 0;
+
+                $incident->save();
+
+                foreach ($row['links'] as $link) {
+                    $evidence = new Evidence();
+                    $evidence->url = $link;
+                    $evidence->incident_id = $incident->id;
+                    $evidence->save();
+                }
+
+            }
+
+
+        }
+    }
+
+    function isValidForCarbon($date)
+    {
+        if (!$date) return false;
+
+        return new Carbon($date) instanceof Carbon;
+    }
+
+}
