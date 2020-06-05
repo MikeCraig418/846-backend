577321825e527309712bd260a0d406bc0e115167
Michael Craig <mcraig@bytelaunch.com>
2020-06-05 09:18:59 -0700

    feat(video): add Video model

--- app/Models/Incident.php
+++ app/Models/Incident.php
@@ -2,9 +2,47 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Incident extends Model
 {
-    //
+    use Uuids;
+    use SoftDeletes;
+
+    public $incrementing = false;
+
+    protected $appends = [
+        'geocoding'
+    ];
+
+    protected $hidden = [
+        'link_1',
+        'link_2',
+        'link_3',
+        'link_4',
+        'link_5',
+        'link_6',
+        'link_7',
+        'lat',
+        'long',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+    protected $casts = [
+        'date' => 'date',
+        'links' => 'array',
+    ];
+
+    public function evidence()
+    {
+        return $this->hasMany('App\Models\Evidence');
+    }
+
+    public function getGeocodingAttribute()
+    {
+        return ['lat' => $this->lat, 'long' => $this->long];
+    }
 }
---
+++ app/Models/Video.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Video extends Model
+{
+    use Uuids;
+    use SoftDeletes;
+
+    public $incrementing = false;
+
+
+    protected $hidden = [
+        'meta',
+        'uploader',
+        'deleted_at'
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+        'streams' => 'array',
+        'meta' => 'array',
+    ];
+
+    public function evidence()
+    {
+        return $this->belongsTo('App\Models\Evidence');
+    }
+}
