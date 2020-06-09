0e4a770f0b4c442cfdcacc12f3cd7bafe003cdba
Michael Craig <mcraig@bytelaunch.com>
2020-06-09 14:01:41 -0700

    feat(legislators): added congressional contacts, including social media accounts

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('incidents', '\App\Http\Controllers\Api\IncidentController@index');
 Route::get('incidents/{id}', '\App\Http\Controllers\Api\IncidentController@show');
+
+Route::get('legislators', '\App\Http\Controllers\Api\LegislatorController@index');
+
+//Route::post('link-submission', '\App\Http\Controllers\Api\LinkSubmissionController@store');
