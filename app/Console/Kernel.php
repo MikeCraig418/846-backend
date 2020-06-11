19d9d5e90850e5c5fe73c04ff848b5cb467f8658
Michael Craig <mcraig@bytelaunch.com>
2020-06-11 16:10:49 -0700

    chore: update cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('pb:run-import')->everyMinute();
         $schedule->command('pb:import-video')->everyThirtyMinutes();
-        $schedule->command('cdn:purge-cache')->everyFifteenMinutes();
+        $schedule->command('cdn:purge-cache')->everyMinute();
     }
 
     /**
