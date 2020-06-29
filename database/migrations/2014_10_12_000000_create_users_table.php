7cb1bc03ce715b407072d63eaab4b1cf90ed362a
Michael Craig <mcraig@bytelaunch.com>
2020-06-28 20:46:24 -0700

    chore: upated readme and some house keeping

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('id', 36)->default('');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
