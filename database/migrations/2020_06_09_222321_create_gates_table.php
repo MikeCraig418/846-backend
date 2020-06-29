e5b468d605b7cfeec884b0f77c422f876836bb89
Michael Craig <mcraig@bytelaunch.com>
2020-06-28 20:49:33 -0700

    chore: upated readme and some house keeping

--- database/migrations/2020_06_09_222321_create_gates_table.php
+++ database/migrations/2020_06_09_222321_create_gates_table.php
@@ -12,39 +12,36 @@ class CreateGatesTable extends Migration
     public function up()
     {
         $tableNames = config('nova-permissions.table_names');
-        
+
         Schema::create($tableNames['roles'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('slug');
-            $table->string('name')->nullable();
-            $table->timestamps();
+            $table->string('slug', 255);
+            $table->string('name', 255)->nullable();
+            $table->nullableTimestamps();
         });
 
         Schema::create($tableNames['role_permission'], function (Blueprint $table) use ($tableNames) {
             $table->unsignedBigInteger('role_id');
-            $table->string('permission_slug');
-            $table->timestamps();
-            $table->foreign('role_id')
-                  ->references('id')
-                  ->on($tableNames['roles'])
-                  ->onDelete('cascade');
+            $table->string('permission_slug', 255);
+            $table->nullableTimestamps();
+
             $table->primary(['role_id', 'permission_slug']);
+
+            $table->foreign(['role_id', 'role_id', 'role_id', 'role_id', 'role_id'], 'role_permission_role_id_foreign')->references(['id', 'id', 'id', 'id', 'id'])->on('roles')->onDelete('CASCADE
+')->onUpdate('RESTRICT');
+
         });
 
         Schema::create($tableNames['role_user'], function (Blueprint $table) use ($tableNames) {
             $table->unsignedBigInteger('role_id');
-            $table->unsignedBigInteger('user_id');
-            $table->timestamps();
-            $table->foreign('role_id')
-                  ->references('id')
-                  ->on($tableNames['roles'])
-                  ->onDelete('cascade');
-        
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on($tableNames['users'])
-                  ->onDelete('cascade');
-            $table->primary(['role_id', 'user_id']);
+            $table->string('user_id', 36)->default('');
+            $table->nullableTimestamps();
+
+
+
+            $table->foreign(['role_id', 'role_id', 'role_id', 'role_id', 'role_id'], 'role_user_role_id_foreign')->references(['id', 'id', 'id', 'id', 'id'])->on('roles')->onDelete('CASCADE
+')->onUpdate('RESTRICT');
+
         });
     }
 
@@ -56,7 +53,7 @@ class CreateGatesTable extends Migration
     public function down()
     {
         $tableNames = config('nova-permissions.table_names');
-        
+
         Schema::dropIfExists($tableNames['role_permission']);
         Schema::dropIfExists($tableNames['role_user']);
         Schema::dropIfExists($tableNames['roles']);
